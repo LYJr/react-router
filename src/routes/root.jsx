8b65e049d474f0dc2cb759cfcd585948d6ce2866
LYJr <lyi91@naver.com>
2023-01-25 15:38:38 +0900

    라우터 에러 해결

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -35,15 +35,7 @@ export default function Root() {
             </form>
           </div>
           <nav>
-            <ul>
-              <li>
-              <Link to={`contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-              <Link to={`contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
-            {contacts.length ? (
+          {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
