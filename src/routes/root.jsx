7da04463a69c7f1b155c5dfddf4944ac5d22e290
LYJr <lyi91@naver.com>
2023-01-25 15:13:21 +0900

    데이터 로드중

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,12 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData, } from "react-router-dom";
+import { getContacts } from "../contacts";
 
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 export default function Root() {
+  const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -37,6 +43,28 @@ export default function Root() {
               <Link to={`contacts/2`}>Your Friend</Link>
               </li>
             </ul>
+            {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
         </div>
         <div id="detail">         
