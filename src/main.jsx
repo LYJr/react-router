60cb3e5c891d5a19d5bc23289e51a5351a4513cf
LYJr <lyi91@naver.com>
2023-01-26 15:40:59 +0900

    로더의 url 매개변수

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
-import Root, { loader as rootLoader }  from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction }  from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader, } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -16,10 +16,12 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
